d17cb22b6625ee40ff77d680f10dc84eca97a59b
JYOTHI KRISHNA <147586507+jyothikrishna1234@users.noreply.github.com>
2024-02-06 14:26:49 +0530

    Add files via upload

---
+++ RandNum.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+public class RandNum
+{
+    public static void main(String ares[])
+    {
+        System.out.println("1 st Random number "+Math.random());
+        System.out.println("2 nd Random number "+Math.random());
+    }
+}
